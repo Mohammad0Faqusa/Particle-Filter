260d5179de78b7c7ee047967b8a4ca19d2fdef43
Mohammad Faqusa <201014@ppu.edu.ps>
2023-12-31 00:31:41 +0200

    the input file is implemented , sample is edited

--- code.cpp
+++ code.cpp
@@ -1,4 +1,7 @@
-#include <iostream> 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <cmath> 
 
 // Name : Mohammad Faqusa 
@@ -24,6 +27,7 @@ class P_filter  {
         float sequareWidth; 
         int sizeArr ; 
         float sumWeight ; 
+        
 
         
         Particle* particlesArr ; 
@@ -33,7 +37,8 @@ class P_filter  {
 
     P_filter () { 
         initMap() ; 
-        initParticlesArr() ; 
+        initParticlesArr() ;
+        initRobotLocation() ; 
     }
 
     void initMap () {
@@ -68,13 +73,13 @@ class P_filter  {
 
     
     void calculate_X_Y(Particle & p , float stepMoved , float angleMoved ) {
-            float angleDegree = (sensor.theta + angleMoved) ; 
+            float angleDegree = (p.theta + angleMoved) ; 
             angleDegree > 360 ? angleDegree - 360 : angleDegree ; 
 
             float angleRadian = angleDegree * (PI / 180) ; 
-            p.x = p.x + cos(angleRadian) ; 
-            p.y = p.y + sin(angleRadian) ; 
-
+            p.x = p.x + stepMoved * cos(angleRadian) ; 
+            p.y = p.y + stepMoved * sin(angleRadian) ; 
+            p.theta = angleDegree ; 
 
             if (p.x > mapWidth )
                 p.x = mapWidth ; 
@@ -88,18 +93,9 @@ class P_filter  {
 
             p.color = getColorSeq(p.x , p.y); 
     }
-    void move() { 
-        float stepMoved  ; 
-        float angleMoved ; 
 
-        float x2 , y2 ; 
-        
-        cout << "Enter the new step distance to move" << endl ; 
-        cin >> stepMoved ; 
-        cout << "Enter the new increasing angle value " << endl ; 
-        cin >> angleMoved ; 
-        
-        calculate_X_Y(sensor ,stepMoved , angleMoved  ); 
+    void move(float stepMoved , float angleMoved ) { 
+
         sensor.color = getColorSeq(sensor.x , sensor.y ) ; 
 
         for (int i = 0 ; i < sizeArr ; i++ ) { 
@@ -125,44 +121,30 @@ class P_filter  {
         particlesArr[i].weight = particlesArr[i].weight / sumWeight ; 
     }
    }
+   void sample() {
+        Particle* resampledParticles = new Particle[sizeArr];
 
-   void sample() { 
-        float * spacePoints = new float [sizeArr] ; 
-        float sum  = 0 ; 
-        for (int i = 0 ; i < sizeArr ; i++ ) { 
-            spacePoints[i] = sum ; 
-            sum += particlesArr[i].weight * sizeArr  ; 
-            
-        }
+        // Roulette wheel resampling
+        float cumulativeWeight = 0.0;
+        float step = sumWeight / sizeArr;
+        float pointer = RandomFloat(0, step);
 
-        int size2 ; 
-        size2 = (int) (0.9 * sizeArr);
-        float r ; 
-        Particle * newParticleArr = new Particle [size2] ; 
-
-        for (int i = 0 ; i < size2 ; i++ ) { 
-            r = RandomFloat (0 , sum ) ;
-            
-
-            for (int j = 0 ; j < sizeArr ; j ++ ) { 
-                if (r >= spacePoints[j]  && r <= spacePoints[j+1]){
-        
-                newParticleArr[i] = particlesArr[j] ; 
-                break ; }
-                
-            
+        for (int i = 0; i < sizeArr; ++i) {
+            cumulativeWeight += particlesArr[i].weight;
+            while (pointer < cumulativeWeight) {
+                resampledParticles[i] = particlesArr[i];
+                pointer += step;
             }
-           
         }
 
-        sizeArr = size2 ; 
-        for (int i = 0 ;  i < sizeArr ; i ++ ) { 
-            particlesArr[i] = newParticleArr[i] ; 
-        }
+        // Deallocate old array
+        delete[] particlesArr;
 
-        
-        
-   }
+        // Update particlesArr with resampled particles
+        particlesArr = resampledParticles;
+    }
+
+
 
 
     
@@ -211,38 +193,89 @@ class P_filter  {
         cout << sensor.x << " " << sensor.y << " " << sensor.theta << " " <<  sensor.color << " " << endl ; 
     }
 
+    void report(){   
+        float meanParticleX, meanParticleY, varParticleX, varParticleY;
+
+        float sum=0;
+	    for (int i=0;i<sizeArr ;i++)
+	    	sum+=particlesArr[i].x;
+            meanParticleX=sum/(float)sizeArr ;
+
+	    sum=0;
+	    for (int i=0;i<sizeArr ;i++)
+	    	sum+=particlesArr[i].y;
+            meanParticleY=sum/(float)sizeArr ;
+
+	    sum=0;
+	    for (int i=0;i<sizeArr ;i++)
+	    	sum+= ((particlesArr[i].x - meanParticleX)*(particlesArr[i].x - meanParticleX));
+
+            varParticleX=sqrt(sum/(float)sizeArr) ;
+	
+	    sum=0;
+	    for (int i=0;i<sizeArr ;i++)
+	        sum+=((particlesArr[i].y - meanParticleY)*(particlesArr[i].y - meanParticleY));
+
+            varParticleY=sqrt(sum/((float)sizeArr )) ;	
+  
+	    cout<< "the mean Particle ( " << meanParticleX<<" , "<<meanParticleY<<") varParticle ( "<<varParticleX<<", "<<varParticleY<<")\n";
+}
+
+
+    void readCSVLine(const string& filename, int lineNumber, float &x, float &y, float &displacement, float &angle) {
+
+        ifstream file(filename);
+        string line;
+
+        for (int i = 1; i <= lineNumber; ++i) {
+            if (!getline(file, line)) {
+                cout << "out of scope\n";
+                return; 
+            }
+            if (i == lineNumber) {
+                istringstream iss(line);
+                string value;
+                int count = 0;
+                while (getline(iss, value, ',')) {
+                    count++;
+                    switch(count) {
+                        case 1: x = stof(value); break;
+                        case 2: y = stof(value); break;
+                        case 3: displacement = stof(value); break;
+                        case 4: angle = stof(value); break;
+                    }
+                }
+                break;
+            }
+        }
+        file.close();
+}
     void play() { 
         char c ; 
-        while (true) {  
-            displayPoints() ;  
+        float stepMoved ; 
+        float angleMoved ; 
+
+        for (int i=0; i<=100; i++){
+            float robotX, robotY, displacement, angle;
+            readCSVLine("./uploads/robot.csv", i+2, sensor.x, sensor.y, stepMoved, angleMoved);
+            move( stepMoved , angleMoved ); 
             displayRobot() ; 
-            move()  ; 
+            report() ;
             updateWeight() ;
             normalize() ;  
             sample() ; 
-            
-            if (sizeArr <= 10) 
-            break ; 
-            cout << "press anyThing to continue , press n to exit" << endl ; 
-            cin >> c  ; 
-            if (c == 'n') {
-            cout << "the operation closed !" << endl ;
-            break; 
-            }
-        }
+       
     }
+    } 
+}; 
 
 
-        
-};
-
-
-int main () 
-
-{ 
+    int main() {
     P_filter app ; 
     app.play() ; 
     
 
-    return 0 ; 
-}
+    return 0 ;
+    }
+    
+
